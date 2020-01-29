39ed2459e95028306671a8c9ce05513e2775dee6
sairam414 <60351014+sairam414@users.noreply.github.com>
2020-01-29 21:05:49 +0530

    changes done

--- prgram.java
+++ prgram.java
@@ -16,3 +16,15 @@ public class AddTwoNumbers2 {
         System.out.println("Sum of these numbers: "+sum);
     }
 }
+
+addition program without scanner
+public class AddTwoNumbers {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
